9a2a3ba4fc3137774404e63b6402145165d5fa4d
scrunchuies <piotruskamilek@gmail.com>
2024-04-26 12:38:17 -0700

    Initial Commit

---
+++ WasteTrackr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WasteTrackr
+//
+//  Created by Piotr Jandura on 4/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
