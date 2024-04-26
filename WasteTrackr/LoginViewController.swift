d66619907840725ca6f462fd9d8913b560773ad7
scrunchuies <piotruskamilek@gmail.com>
2024-04-26 14:27:07 -0700

    Basic login screen that sends user to blank home screen. Email and password used to login.

--- WasteTrackr/LoginViewController.swift
+++ WasteTrackr/LoginViewController.swift
@@ -6,16 +6,68 @@
 //
 
 import UIKit
+import Firebase
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        emailTextField.delegate = self
+        passTextField.delegate = self
+        emailTextField.layer.borderWidth = 1
+        emailTextField.layer.cornerRadius = 8
+        passTextField.layer.borderWidth = 1
+        passTextField.layer.cornerRadius = 8
+        
+        let emailPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        emailTextField.attributedPlaceholder = emailPlaceholder
+        let passPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        passTextField.attributedPlaceholder = passPlaceholder
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == passTextField {
+            textField.resignFirstResponder() // Dismiss the keyboard
+            loginClicked(()) // Trigger the login action without any arguments
+        } else {
+            passTextField.becomeFirstResponder() // Move to the next text field
+        }
+        return true
+    }
+    
+    @objc func dismissKeyboard() {
+            view.endEditing(true)
+        }
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        guard let email = emailTextField.text, !email.isEmpty else {
+            print("Email is empty")
+            return
+        }
+        guard let pass = passTextField.text, !pass.isEmpty else {
+            print("Password is empty")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: pass) { [weak self] (authResult, error) in
+            if let error = error {
+                print("Error signing in:", error.localizedDescription)
+                return
+            }
+            
+            // Authentication successful, perform segue
+            self?.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
 
+    
+    
     /*
     // MARK: - Navigation
 
