493315b6e1752ca8df0e0f9fe0703f854c539aa3
scrunchuies <piotruskamilek@gmail.com>
2024-04-28 18:06:58 -0700

    uhhhhh... supa sync

--- WasteTrackr/LoginViewController.swift
+++ WasteTrackr/LoginViewController.swift
@@ -11,6 +11,7 @@ import Firebase
 class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passTextField: UITextField!
+    @IBOutlet weak var rememberMeCheckbox: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +30,16 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tapGesture)
+        
+        // Check if "Remember Me" is selected
+        let rememberMe = UserDefaults.standard.bool(forKey: "RememberMe")
+        rememberMeCheckbox.isOn = rememberMe
+        
+        // If "Remember Me" is selected, populate email and password fields with saved values
+        if rememberMe {
+            emailTextField.text = UserDefaults.standard.string(forKey: "SavedEmail")
+            passTextField.text = UserDefaults.standard.string(forKey: "SavedPassword")
+        }
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -42,8 +53,22 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     @objc func dismissKeyboard() {
-            view.endEditing(true)
+        view.endEditing(true)
+    }
+    
+    @IBAction func rememberMeCheckboxToggled(_ sender: UISwitch) {
+        UserDefaults.standard.set(sender.isOn, forKey: "RememberMe")
+        
+        if sender.isOn {
+            // If "Remember Me" is selected, save the email and password
+            UserDefaults.standard.set(emailTextField.text, forKey: "SavedEmail")
+            UserDefaults.standard.set(passTextField.text, forKey: "SavedPassword")
+        } else {
+            // If "Remember Me" is deselected, clear saved email and password
+            UserDefaults.standard.removeObject(forKey: "SavedEmail")
+            UserDefaults.standard.removeObject(forKey: "SavedPassword")
         }
+    }
     
     @IBAction func loginClicked(_ sender: Any) {
         guard let email = emailTextField.text, !email.isEmpty else {
@@ -65,17 +90,18 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             self?.performSegue(withIdentifier: "loginToHome", sender: self)
         }
     }
-
+    
     
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
--- WasteTrackr/TrackingViewController.swift
+++ WasteTrackr/TrackingViewController.swift
@@ -10,10 +10,14 @@ import FirebaseFirestore
 
 class TrackingViewController: UIViewController {
     @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    private var name = "" // Initial value for name
     private var count = 0 // Initial count value
     private let db = Firestore.firestore()
     private let collectionRef = Firestore.firestore().collection("cfa") // Replace "your_collection_name" with your actual collection name
     private var countListener: ListenerRegistration?
+    private var nameListener: ListenerRegistration?
     
     
     override func viewDidLoad() {
@@ -21,27 +25,51 @@ class TrackingViewController: UIViewController {
         // Load initial count from Firestore
         loadCount()
         startCountListener()
+        startNameListener()
+        
+        nameTextField.addTarget(self, action: #selector(nameTextFieldDidChange(_:)), for: .editingChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
     }
     
     deinit {
-            countListener?.remove()
-        }
-        
-        private func startCountListener() {
-            countListener = collectionRef.document("FTnn41nFjfyLN2ZdQr90").addSnapshotListener { [weak self] (documentSnapshot, error) in
-                guard let self = self else { return }
-                guard let document = documentSnapshot, document.exists else {
-                    print("Count document does not exist")
-                    return
-                }
-                if let count = document.data()?["count"] as? Int {
-                    self.count = count
-                    self.updateCountLabel()
-                } else {
-                    print("Invalid count value")
-                }
+        countListener?.remove()
+        nameListener?.remove()
+    }
+    
+    private func startCountListener() {
+        countListener = collectionRef.document("FTnn41nFjfyLN2ZdQr90").addSnapshotListener { [weak self] (documentSnapshot, error) in
+            guard let self = self else { return }
+            guard let document = documentSnapshot, document.exists else {
+                print("Count document does not exist")
+                return
+            }
+            if let count = document.data()?["count"] as? Int {
+                self.count = count
+                self.updateCountLabel()
+                self.updateNameTextField()
+            } else {
+                print("Invalid count value")
             }
         }
+    }
+    
+    private func startNameListener() {
+        nameListener = collectionRef.document("FTnn41nFjfyLN2ZdQr90").addSnapshotListener { [weak self] (documentSnapshot, error) in
+            guard let self = self else { return }
+            guard let document = documentSnapshot, document.exists else {
+                print("Document does not exist")
+                return
+            }
+            if let name = document.data()?["name"] as? String {
+                self.name = name
+                self.updateNameTextField()
+            } else {
+                print("Invalid name value")
+            }
+        }
+    }
     
     // Load count value from Firestore
     private func loadCount() {
@@ -55,6 +83,7 @@ class TrackingViewController: UIViewController {
             } else {
                 if let document = document, document.exists, let count = document.data()?["count"] as? Int {
                     self.count = count
+                    self.updateNameTextField()
                 } else {
                     print("Count document does not exist or count value is invalid.")
                     // Provide a default value for count
@@ -63,12 +92,35 @@ class TrackingViewController: UIViewController {
             }
             
             self.updateCountLabel()
+            self.updateNameTextField()
+        }
+    }
+    
+    private func loadName() {
+        collectionRef.document("FTnn41nFjfyLN2ZdQr90").getDocument { [weak self] (document, error) in
+            guard let self = self else { return }
+            
+            if let error = error {
+                print("Error getting name value: \(error.localizedDescription)")
+                // Provide a default value for name
+                self.name = "" // You can set it to any default value you prefer
+            } else {
+                if let document = document, document.exists, let name = document.data()?["name"] as? String {
+                    self.name = name
+                } else {
+                    print("Document does not exist or name value is invalid.")
+                    // Provide a default value for name
+                    self.name = "" // You can set it to any default value you prefer
+                }
+            }
+            
+            self.updateNameTextField()
         }
     }
     
     // Update count value in Firestore
     private func updateCountInFirestore() {
-        collectionRef.document("FTnn41nFjfyLN2ZdQr90").setData(["count": count]) { error in
+        collectionRef.document("FTnn41nFjfyLN2ZdQr90").setData(["name": name, "count": count]) { error in
             if let error = error {
                 print("Error updating count value: \(error.localizedDescription)")
             } else {
@@ -81,6 +133,42 @@ class TrackingViewController: UIViewController {
     private func updateCountLabel() {
         countLabel.text = "\(count)"
     }
+    
+    // Update name value in Firestore
+    private func updateNameInFirestore() {
+        collectionRef.document("FTnn41nFjfyLN2ZdQr90").setData(["name": name, "count": count]) { error in
+                if let error = error {
+                    print("Error updating values: \(error.localizedDescription)")
+                } else {
+                    print("Values updated successfully")
+                }
+            }
+    }
+    
+    
+    // Update text field with current name value
+    private func updateNameTextField() {
+        nameTextField.text = name
+    }
+    
+    
+    @objc private func nameTextFieldDidChange(_ textField: UITextField) {
+        // Update name value in Firestore whenever the text field changes
+        if let newName = textField.text {
+            name = newName
+            updateNameInFirestore()
+        }
+    }
+    
+    
+    
+    
+    // ---------------------------------------------------------
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
     
     // Action method for button tap
     @IBAction private func incrementButtonTapped(_ sender: UIButton) {
@@ -127,8 +215,53 @@ class TrackingViewController: UIViewController {
             }
         }
     }
-
-
+    
+    // Add this IBAction method for the decrement button tap
+    @IBAction private func decrementButtonTapped(_ sender: UIButton) {
+        // Decrement count using a transaction
+        db.runTransaction({ (transaction, errorPointer) -> Any? in
+            // Get the latest count value
+            let countDocumentRef = self.collectionRef.document("FTnn41nFjfyLN2ZdQr90")
+            let countDocumentSnapshot: DocumentSnapshot
+            do {
+                try countDocumentSnapshot = transaction.getDocument(countDocumentRef)
+            } catch let fetchError as NSError {
+                errorPointer?.pointee = fetchError
+                return nil
+            }
+            
+            guard let countData = countDocumentSnapshot.data(),
+                  var currentCount = countData["count"] as? Int else {
+                let error = NSError(domain: "AppErrorDomain", code: -1, userInfo: [
+                    NSLocalizedDescriptionKey: "Count value is missing or invalid"
+                ])
+                errorPointer?.pointee = error
+                return nil
+            }
+            
+            // Decrement the count
+            currentCount -= 1
+            
+            // Update the count in Firestore
+            transaction.updateData(["count": currentCount], forDocument: countDocumentRef)
+            
+            return currentCount
+        }) { [weak self] (count, error) in
+            guard let self = self else { return }
+            if let error = error {
+                print("Transaction failed: \(error.localizedDescription)")
+            } else {
+                if let newCount = count as? Int {
+                    // Update local count and label
+                    self.count = newCount
+                    self.updateCountLabel()
+                } else {
+                    print("Invalid count value received from transaction")
+                }
+            }
+        }
+    }
+    
     
     
     /*
